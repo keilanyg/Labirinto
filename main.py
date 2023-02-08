412967b9dada6f5045adb8a18614b6664d4af6f1
keilanyg <keilanydes@gmail.com>
2023-02-08 19:40:00 -0300

    Labirinto

---
+++ main.py
@@ -0,0 +1,101 @@
+from ast import IfExp
+import os
+
+FIM_LINHA = [20]
+FIM_COLUNA = [19]
+
+
+labirinto = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1]
+
+]
+
+
+class Maze:
+    def __init__(self):
+        self.l = 1
+        self.c = 0
+
+    def formalabirinto(self):
+        lab_impressao = []
+        for i, linha in enumerate(labirinto):
+            for j, coluna in enumerate(labirinto[i]):
+                if labirinto[i][j] == 0:
+                    lab_impressao = ' '
+                elif labirinto[i][j] == 1:
+                    lab_impressao = '|'
+                elif labirinto[i][j] == 2:
+                    lab_impressao = 'x'
+                elif labirinto[i][j] == 3:
+                    lab_impressao = '.'
+                print(lab_impressao, end='')
+            print()
+
+    def andarlabirinto(self):
+        os.system("cls")
+        esquerda = labirinto[self.l][self.c-1]
+        direita = labirinto[self.l][self.c+1]
+        cima = labirinto[self.l-1][self.c]
+        baixo = labirinto[self.l+1][self.c]
+
+        if direita == 0:
+            self.c += 1
+            labirinto[self.l][self.c] = 2
+        elif esquerda == 0:
+            self.c -= 1
+            labirinto[self.l][self.c] = 2
+        elif baixo == 0:
+            self.l += 1
+            labirinto[self.l][self.c] = 2
+        elif cima == 0:
+            self.l -= 1
+            labirinto[self.l][self.c] = 2
+        elif direita == 2:
+            self.c += 1
+            labirinto[self.l][self.c] = 2
+            labirinto[self.l][self.c-1] = 3
+        elif (esquerda == 2):
+            self.c -= 1
+            labirinto[self.l][self.c] = 2
+            labirinto[self.l][self.c+1] = 3
+        elif (baixo == 2):
+            self.l += 1
+            labirinto[self.l][self.c] = 2
+            labirinto[self.l-1][self.c] = 3
+        elif cima == 2:
+            self.l -= 1
+            labirinto[self.l][self.c] = 2
+            labirinto[self.l+1][self.c] = 3
+
+    def automatico(self):
+        while self.l != FIM_LINHA or self.c != FIM_COLUNA:
+            self.formalabirinto()
+            print('\n')
+            self.andarlabirinto()
+
+    def run(self):
+        self.automatico()
+
+
+Lab = Maze()
+Lab.run()
